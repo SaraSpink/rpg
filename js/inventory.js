8f727e5f124572d5688563322833785c532720f7
Aaron Rohrbacher and Sara Spink <aaron@>
2017-10-11 15:12:22 -0700

    work on adding values

--- js/inventory.js
+++ js/inventory.js
@@ -1,13 +1,13 @@
 
 export class Inventory {
-  constructor() {3
-    this.chordInventory = ['C', 'G', 'A', 'Zminor']
-    this.chordPoints = [1, 1, 2, 1000]
+  constructor() {
+    this.chordInventory = ['A', 'Em', 'D', 'C#M', 'Zm']
+    this.chordPoints = [2, 5, 2, 10, 1000]
 
-    this.instrumentInventory = ['Ukelele', 'Marimba', 'Guitar', 'Sax', 'Kazoo', 'Gamelan']
-    this.instrumentPoints = [1, 3, 4, 500, -2, 50]
+    this.instrumentInventory = ['Guitar', 'Sweet Guitar', 'FLUTE', 'Clarinet', 'Violin', 'Gamelan']
+    this.instrumentPoints = [-20, -40, -100, -200, -500, -10]
 
     this.extrasInventory = ['tuner', 'metronome', 'whammy bar', 'studio time', 'demo tape']
-    this.extrasPoints = [5, 2, 70, 100, 10000]
+    this.extrasPoints = [5, 5, 70, 100, 150]
   }
 }
