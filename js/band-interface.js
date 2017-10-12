cb08dbac8d6f107ba066b42c12a16f7a3d70c72f
Aaron Rohrbacher and Sara Spink <sara@>
2017-10-12 10:01:51 -0700

    add more user stories

--- js/band-interface.js
+++ js/band-interface.js
@@ -1,14 +1,16 @@
 import { Band } from './../js/band.js';
 import { Inventory } from './../js/inventory.js';
 $(document).ready(function() {
-  let band = new Band(name);
-  $('#balance').text(band.addPoints(band.points));
-  $('#instruments').text(band.instruments);
-
+  let band;
   $('#bandNameInput').submit(function(event) {
     event.preventDefault();
     let name = $("#name").val()
-    console.log(band.addPoints(band.points))
+    band = new Band(name);
+
+    $('#status').slideDown();
+    $('#balance').text(band.addPoints(band.points));
+    $('#instruments').text(band.instruments);
+    $('#chords').text(band.chords)
     $('#welcome').slideUp();
     $('#bandName').text(band.name);
     $('#beginning').show();
@@ -28,15 +30,70 @@ $(document).ready(function() {
   $('#peteInstruments').submit(function(event) {
     event.preventDefault();
     let instrument = $("input:radio[name=instrument]:checked").val();
-    console.log(instrument);
     $('#pete').slideUp();
-    band.addInstrument(instrument)
-    $('#balance').text(band.addPoints(band.points));
-    $('#instruments').text(band.instruments);
-    if (band.addPoints(band.points) < 1) {
+    if (instrument !== "6") {
+      band.addInstrument(instrument)
+      $('#balance').text(band.addPoints(band.points));
+      $('#instruments').text(band.instruments);
+      $('#chords').text(band.chords)
+    } else if  (band.addPoints(band.points) < 1){
       $('#loser').show();
     } else {
-      $('#practice').show();
+      $('#practiceBar').show();
     };
   });
+
+  $('#archibaldInstruments').submit(function(event) {
+    event.preventDefault();
+    let instrument = $("input:radio[name=instrument]:checked").val();
+    $('#archibald').slideUp();
+    if (instrument !== "6") {
+      band.addInstrument(instrument)
+      $('#balance').text(band.addPoints(band.points));
+      $('#instruments').text(band.instruments);
+      $('#chords').text(band.chords)
+    } else if  (band.addPoints(band.points) < 1){
+      $('#loser').show();
+    } else {
+      $('#practiceBar').show();
+    };
+  });
+
+  $('#practiceBar').submit(function(event) {
+    event.preventDefault();
+    $('#practiceBar').fadeOut()
+    let practiceChoice =
+    $("input:radio[name=practiceChoice]:checked").val();
+    if (practiceChoice === "practice") {
+      $('#practice').slideDown();
+    } else {
+      $('#bar').slideDown();
+    }
+  })
+
+  $('#practiceForm').submit(function(event) {
+    event.preventDefault();
+    $('#practice').fadeOut()
+    let practiceTime = $("input:radio[name=practiceTime]:checked").val();
+    if (practiceTime === "stairway") {
+      $('#noStairway').slideDown()
+    } else if (practiceTime = "circleOfFifths") {
+      band.addChord(0)
+      band.addChord(1)
+      alert("You just added two chords to your library. Welcome to stardom! Your gigs will be worth more money now. Fuckin' epic, dude or dudette!")
+      $('#home').slideDown()
+      $('#balance').text(band.addPoints(band.points));
+      $('#instruments').text(band.instruments);
+      $('#chords').text(band.chords)
+    } else {
+      band.addChord(2)
+      alert("You just a chord to your library. Way to play! Your gigs will be worth more money now. Fuckin' epic, dude or dudette!")
+      $('#home').slideDown()
+      $('#balance').text(band.addPoints(band.points));
+      $('#instruments').text(band.instruments);
+      $('#chords').text(band.chords)
+    }
+
+  })
+
 });
